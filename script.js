4631da2f63b46c3538c214191e12fb60d7cf4685
faiza203 <fiazfaiza981@gmail.com>
2020-12-31 11:55:06 -0500

    Add function to get new Quote

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const getNewQuotes = (qoutesLength) => {
-    const rNum = Math.floor(Math.random() * qoutesLength)
-    return rNum;
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+let realData = "";
+
+const getNewQuotes = () => {
+    const rNum = Math.floor(Math.random() * realData.length);
+    quote.innerText = realData[rNum].text;
+    author.innerText = realData[rNum].author;
 }
+
 const getQuotes = async() => {
     const api = "https://type.fit/api/quotes";
-    let realData;
     try {
         let data = await fetch(api);
         realData = await data.json();
-        const quote = getNewQuotes(realData.length);
-        console.log(realData[quote]);
+        getNewQuotes(realData);
     } catch (err) { console.log(err.message); }
 }
 
