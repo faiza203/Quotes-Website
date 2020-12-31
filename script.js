790087709b9de574e51099239e76b018f9e28385
faiza203 <fiazfaiza981@gmail.com>
2020-12-31 11:43:18 -0500

    Get Random Quote Successfully

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+const getNewQuotes = (qoutesLength) => {
+    const rNum = Math.floor(Math.random() * qoutesLength)
+    return rNum;
+}
 const getQuotes = async() => {
     const api = "https://type.fit/api/quotes";
+    let realData;
     try {
         let data = await fetch(api);
-        let realData = await data.json();
-        console.log(realData[0].author);
-    } catch (err) { console.log(err); }
+        realData = await data.json();
+        const quote = getNewQuotes(realData.length);
+        console.log(realData[quote]);
+    } catch (err) { console.log(err.message); }
 }
 
 getQuotes();
