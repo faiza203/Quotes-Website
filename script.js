8f738438a2e34ce6051dca8e587773bca3691e4b
faiza203 <fiazfaiza981@gmail.com>
2021-01-05 14:52:51 -0500

    Create app.js

--- script.js
+++ script.js
@@ -4,25 +4,26 @@ let realData = "";
 let quoteData = "";
 
 const tweetNow = () => {
-    let tweetPost = `https://twitter.com/intent/tweet?text=${quoteData.text}`;
-    window.open(tweetPost)
-}
+  let tweetPost = `https://twitter.com/intent/tweet?text=${quoteData.text}`;
+  window.open(tweetPost);
+};
 
 const getNewQuotes = () => {
-    const rNum = Math.floor(Math.random() * realData.length);
-    quoteData = realData[rNum];
-    quote.innerText = quoteData.text;
-    author.innerText = quoteData.author ? "&" +
-        quoteData.author : "UnKnown";
-}
+  const rNum = Math.floor(Math.random() * realData.length);
+  quoteData = realData[rNum];
+  quote.innerText = quoteData.text;
+  author.innerText = quoteData.author ? "&" + quoteData.author : "UnKnown";
+};
 
-const getQuotes = async() => {
-    const api = "https://type.fit/api/quotes";
-    try {
-        let data = await fetch(api);
-        realData = await data.json();
-        getNewQuotes(realData);
-    } catch (err) { console.log(err.message); }
-}
+const getQuotes = async () => {
+  const api = "https://type.fit/api/quotes";
+  try {
+    let data = await fetch(api);
+    realData = await data.json();
+    getNewQuotes(realData);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
 getQuotes();
